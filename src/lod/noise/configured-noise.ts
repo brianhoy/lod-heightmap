28579e38bdc488295b1add7a34215e29212fb7fc
Brian Hoy <brianhoy4@gmail.com>
2016-09-22 15:32:42 -0400

    changes

--- src/lod/noise/configured-noise.ts
+++ src/lod/noise/configured-noise.ts
@@ -1,15 +1,16 @@
 import { Noise } from './noise';
+import { NoiseModel } from './noise-model';
 
 export class ConfiguredNoise {
 	private _noise: Noise;
 
 	constructor() {
-		this._noise = new Noise(JSON.parse(`
-{"type":"simplex","baseHeight":100,"octaves":4,"persistence":0.5,"lancunarity":2,"frequency":0.4,"low":0,"high":5000,"operation":"multiply","children":[{"type":"simplex","baseHeight":100,"octaves":1,"persistence":0.5,"lancunarity":2,"frequency":0.1,"low":0,"high":1,"operation":"add","children":[],"uid":0.8910968614721209}],"uid":0.6775682121284541}
-		`));
+		this._noise = new Noise(new NoiseModel("simplex", 0, 4, 0.5, 1, 0.9, 0, 1, "add", []));
 	}
 
 	public noise(x: number, y: number): number {
-		return this._noise.noise(x, y); 
+		let noise = this._noise.noise(x, y);
+		if(Math.random() > 0.99) console.log(noise);
+		return noise;
 	}
 }
